1144941e28c4c9295b8f9b6c36b3f0a7b565fca8
spaunova <misspaunova@gmail.com>
2016-09-11 11:58:57 +0200

    Add empty services for client

---
+++ client/app/scripts/services/categoryservice.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name expenseTrackerApp.categoryService
+ * @description
+ * # categoryService
+ * Service in the expenseTrackerApp.
+ */
+angular.module('expenseTrackerApp')
+  .service('categoryService', function () {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+  });
---
+++ client/app/scripts/services/expenseservice.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name expenseTrackerApp.expenseService
+ * @description
+ * # expenseService
+ * Service in the expenseTrackerApp.
+ */
+angular.module('expenseTrackerApp')
+  .service('expenseService', function () {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+  });
